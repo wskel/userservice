4c5f68d5de0a7f8596c21d28f2538a8a5fb9b620
Will Skelly <60189821+wskel@users.noreply.github.com>
2024-02-25 00:39:04 -0500

    Add support for local and dev spring profiles

--- src/main/kotlin/com/github/wskel/userservice/Application.kt
+++ src/main/kotlin/com/github/wskel/userservice/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class UserserviceApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<UserserviceApplication>(*args)
+    runApplication<Application>(*args)
 }
